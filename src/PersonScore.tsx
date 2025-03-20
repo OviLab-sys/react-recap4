05dce3459369b6d60217322e018cbaad3186e6f5
vicoduor <victoroduorr@gmail.com>
2025-03-20 22:52:44 +0300

    .

--- src/PersonScore.tsx
+++ src/PersonScore.tsx
@@ -1,15 +1,30 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPerson } from './getPerson';
 
 export function PersonScore() {
+  const [name, setName] = useState<string | undefined>();
+  const [score, setScore] = useState(0);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    async function getThePerson() {
-      const person = await getPerson();
-      console.log(person);
-    }
-    getThePerson();
+    getPerson().then((person) => {
+      setLoading(false);
+      setName(person.name);
+    });
   }, []);
-  return null;
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {name}, {score}
+      </h3>
+      <button onClick={() => setScore(score + 1)}>Add</button>
+      <button onClick={() => setScore(score - 1)}>Subtract</button>
+      <button onClick={() => setScore(0)}>Reset</button>
+    </div>
+  );
 }
 
 //The effect calls the getPerson function and outputs the returned person to the console.
